51a79276f6846ebb7b30e8db6654ce8aada27361
Tcll <tcll5850@gmail.com>
2018-04-07 10:57:29 -0400

    / ‘Plugins/BBCPP1/stable.js’ / ‘projectfiles/Tcll/.idea/workspace.xml’

--- Plugins/BBCPP1/stable.js
+++ Plugins/BBCPP1/stable.js
@@ -176,14 +176,14 @@ $(document).ready(function() {
                 container.css('display','inline-block');
                 var CCSStext = container.children('textarea');
                 CCSStext.after(
-                    '<div id="Tcll5850-PS-classes" class="" style="overflow-y:auto; margin: 0px; margin-top: 5px; padding: 2px; padding-left:5px;' +
+                    '<div id="Tcll5850-PS-classes" class="" style="overflow-y:auto; margin: 0px; margin-top: 5px; padding: 2px; padding-left:5px; margin-left: 10px;' +
                     ' border-bottom; 3px solid gray; border-width: 1px; border-style: solid; border-color: #606060; border-radius: 5px;"></div>');
                 var preview = $('div#Tcll5850-PS-classes');;
                 preview.width(CCSStext.width());
                 preview.height(CCSStext.height());
                 CCSStext.css('border-bottom', '3px solid gray');
                 //CCSStext.css('float', 'left');
-                CCSStext.css('margin-right', '10px');
+                //CCSStext.css('margin-right', '10px');
                 CCSStext.keyup(function() {
                     ss.text( $('div.custom-field-customcss').children('textarea').val() );
                     var pstyle = ss[0].sheet;
