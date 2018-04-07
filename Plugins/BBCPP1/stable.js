c887498f4b1f43b39685114b7c7e01d2f68e580a
Tcll <tcll5850@gmail.com>
2018-04-07 13:19:56 -0400

    / ‘Plugins/BBCPP1/stable.js’ / ‘projectfiles/Tcll/.idea/workspace.xml’

--- Plugins/BBCPP1/stable.js
+++ Plugins/BBCPP1/stable.js
@@ -178,7 +178,7 @@ $(document).ready(function() {
                 var CCSStext = container.children('textarea');
                 CCSStext.after(
                     '<div id="Tcll5850-PS-classes" class="" style="overflow-y:auto; margin: 0px; margin-top: 5px; padding: 2px; padding-left:5px;' +
-                    ' border-width: 1px; border-style: solid; border-color: #606060; border-radius: 5px; border-bottom; 3px solid gray;"></div>');
+                    ' border-width: 1px; border-style: solid; border-color: #606060; border-radius: 5px; border-bottom: 3px solid gray;"></div>');
                 var preview = $('div#Tcll5850-PS-classes');
                 preview.width(CCSStext.width());
                 preview.height(CCSStext.height());
